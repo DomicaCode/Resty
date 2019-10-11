4d6314bdfbeb8ddd216efc020bd24502dc8a8476
Domica <dokiiscool35@gmail.com>
2019-10-11 19:08:36 +0200

    minor fixes

--- Resty/Controllers/HomeController.cs
+++ Resty/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Resty.Models;
 
 namespace Resty.Controllers
 {
@@ -13,12 +12,6 @@ namespace Resty.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
